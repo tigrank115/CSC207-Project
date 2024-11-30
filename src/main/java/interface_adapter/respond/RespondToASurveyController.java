8ad5c8a9cea905e15cc40b171e5a2be0c6bf2a12
Kevin Po <kevinpo>
2024-11-30 13:04:05 -0500

    merged with tigrak115 data-access-layer branch and updated RepondToASurveyController

--- src/main/java/interface_adapter/respond/RespondToASurveyController.java
+++ src/main/java/interface_adapter/respond/RespondToASurveyController.java
@@ -1,7 +1,29 @@
 package interface_adapter.respond;
 
+import use_case.make_response.MakeResponseInputBoundary;
+import use_case.make_response.MakeResponseInputData;
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInputData;
+
 /**
  * Controller for the Respond to a survey Use Case.
  */
 public class RespondToASurveyController {
+
+    private final MakeResponseInputBoundary userMakeResponseUseCaseInteractor;
+
+    public RespondToASurveyController(MakeResponseInputBoundary userMakeResponseUseCaseInteractor) {
+        this.userMakeResponseUseCaseInteractor = userMakeResponseUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Respond to a survey Use Case.
+     */
+    public void execute(String username, String password1, String password2) {
+        final SignupInputData signupInputData = new SignupInputData(
+                username, password1, password2);
+
+        userSignupUseCaseInteractor.execute(signupInputData);
+    }
+
 }
