fd7eb30126213d3d4cf4116fd09c9c781602b8cb
tigrank115 <tigran.shatmets@gmail.com>
2024-11-30 14:09:47 -0500

    Refactored generation of sample data.

--- src/test/java/use_case/get_responses/GetResponsesInteractorTest.java
+++ src/test/java/use_case/get_responses/GetResponsesInteractorTest.java
@@ -4,10 +4,8 @@ import data_access.DBSurveyDataAccessObject;
 import data_access.InMemorySurveyDataAccessObject;
 import entity.*;
 import org.junit.jupiter.api.Test;
+import use_case.SampleDataGenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,20 +32,7 @@ public class GetResponsesInteractorTest {
 
     @Test
     public void getResponsesSuccessTest() {
-        Survey survey = new Survey("Mood Survey");
-        survey.addQuestion(new MultipleChoiceQuestion(
-                "How are you?",
-                Arrays.asList("Good", "So-so", "Bad"),
-                true,
-                true
-        ));
-        survey.addQuestion(new TextQuestion(
-                "Describe yourself.",
-                true
-        ));
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2025, Calendar.JANUARY, 1, 0, 0, 0);
-        survey.setCloseDate(calendar.getTime());
+        Survey survey = SampleDataGenerator.newSurvey();
 
         // Recall: this example implements GetResponsesDataAccessInterface.
         InMemorySurveyDataAccessObject responseDAO = new InMemorySurveyDataAccessObject();
@@ -55,11 +40,7 @@ public class GetResponsesInteractorTest {
         // Store the Survey.
         responseDAO.saveSurvey(survey);
 
-        // Response-related setup
-        ArrayList<Answer> answers = new ArrayList<>();
-        answers.add(new Answer(AnswerType.SINGLE_CHOICE, new String[]{"Good"}));
-        answers.add(new Answer(AnswerType.TEXT, new String[]{"Everything is peachy."}));
-        Response response = new Response(answers);
+        Response response = SampleDataGenerator.newResponse();
 
         responseDAO.saveResponse(response, "0");
 
