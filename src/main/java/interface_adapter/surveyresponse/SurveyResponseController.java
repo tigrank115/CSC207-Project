ff128bd303a37d911c16e45ccbed3fe3a79c6212
SweetOak <78226455+SweetOak@users.noreply.github.com>
2024-12-02 01:25:59 -0500

    Survey response controller

--- src/main/java/interface_adapter/surveyresponse/SurveyResponseController.java
+++ src/main/java/interface_adapter/surveyresponse/SurveyResponseController.java
@@ -1,4 +1,14 @@
 package interface_adapter.surveyresponse;
 
+import use_case.get_survey.GetSurveyInputData;
+
 public class SurveyResponseController {
+    public void execute(String username, String surveyID) {
+        final GetSurveyInputData getSurveyInputData = new GetSurveyInputData(
+                username, surveyID);
+
+
+
+
+    }
 }
--- src/main/java/interface_adapter/surveyresponse/SurveyResponsePresenter.java
+++ src/main/java/interface_adapter/surveyresponse/SurveyResponsePresenter.java
@@ -1,4 +1,18 @@
 package interface_adapter.surveyresponse;
 
+import use_case.login.LoginOutputData;
+
 public class SurveyResponsePresenter {
+
+    public void switchToLoginView() {
+
+    }
+
+    public void switchToRespondSurveyView(LoginOutputData loginOutputData) {
+
+    }
+
+    public void switchToCreateSurveyView(LoginOutputData loginOutputData) {
+
+    }
 }
